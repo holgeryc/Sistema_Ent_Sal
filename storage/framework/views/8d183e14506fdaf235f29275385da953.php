c906746b89429ffe41173631ae081ed9149c3f6f
Holger Alfredo Yupanqui Carrillo <h0lg3r@hotmail.com>
2023-04-18 16:19:50 -0500

    vendor

---
+++ storage/framework/views/8d183e14506fdaf235f29275385da953.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema - <?php echo $__env->yieldContent('title'); ?></title>
+    <meta name="description" content="<?php echo $__env->yieldContent('meta-description', 'Default meta description'); ?>">
+</head>
+<body>
+    <?php echo $__env->make('Navegacion.navigation', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <!-- Imprimimos el contenido de la variable de cada página -->
+
+    <?php echo $__env->yieldContent('content'); ?>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\Sistema_Ent_Sal\resources\views/Diseños/app.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/a62d1ae1264615656183564845e01fd7.php
@@ -0,0 +1,12 @@
+
+
+<?php $__env->startSection('title', 'Inicio'); ?>
+
+<?php $__env->startSection('meta-description', 'Inicio meta description'); ?>
+
+
+<?php $__env->startSection('content'); ?>
+    <h1>Inicio</h1>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('Diseños.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Sistema_Ent_Sal\resources\views/welcome.blade.php ENDPATH**/ ?>

1261151636b9bd7ef02bb272d58eb0363435ffe8
Holger Alfredo Yupanqui Carrillo <h0lg3r@hotmail.com>
2023-04-20 17:28:46 -0500

    add

--- resources/views/entrada.blade.php
+++ resources/views/entrada.blade.php
@@ -7,6 +7,6 @@
 @section('content')
     <h1>Entrada</h1>
     @foreach($posts as $post)
-        @dump($post);
+        <h1>{{$post-> correo}}</h1>
     @endforeach
 @endsection
--- storage/framework/views/aad75efa0ba5486093ff8cfbca05fe48.php
+++ storage/framework/views/aad75efa0ba5486093ff8cfbca05fe48.php
@@ -7,7 +7,7 @@
 <?php $__env->startSection('content'); ?>
     <h1>Entrada</h1>
     <?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <?php dump($post); ?>;
+        <h1><?php echo e($post-> correo); ?></h1>
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('Diseños.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Sistema_Ent_Sal\resources\views/Entrada.blade.php ENDPATH**/ ?>
